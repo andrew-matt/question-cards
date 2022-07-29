425773d3a114b86f25558395b3e07bdc75fc6f8a
andrew-matt <andrew.matt@ya.ru>
2022-07-29 17:47:40 +0300

    change URL to heroku

--- src/features/auth/profile/Profile-API.ts
+++ src/features/auth/profile/Profile-API.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const instance= axios.create({
     withCredentials:true,
-     baseURL:'http://localhost:7542/2.0/',
+     baseURL:'https://neko-back.herokuapp.com/2.0',
     // headers: {
     //     "API-KEY": "bb4d8d53-c64d-4047-9be6-a6b7671d5984"
     // }
