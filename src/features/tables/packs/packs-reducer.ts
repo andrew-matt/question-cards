30f233f73ef79d92d931345c25fb07947227e6cf
andrew-matt <andrew.matt@ya.ru>
2022-08-01 08:46:27 +0300

    add pagination to PacksList

--- src/features/tables/packs/packs-reducer.ts
+++ src/features/tables/packs/packs-reducer.ts
@@ -1,16 +1,24 @@
-import {AxiosError} from 'axios';
 import {AppThunk} from '../../../app/store';
-import {setAppErrorAC, setAppRequestStatusAC} from '../../../app/app-reducer';
+import {setAppRequestStatusAC} from '../../../app/app-reducer';
 import {packsAPI, ResponseCardPackType, UpdatePackParamsType} from './packs-api';
+import {handleServerNetworkError} from '../../../utils/error-utils';
 
-const initialState: ResponseCardPackType[] = [];
+const _initialState: ResponseCardPackType[] = [];
+const initialState = {
+    packsList: [] as ResponseCardPackType[],
+    requestedPacks: `User's` as RequestedPacksType,
+};
 
 export const packsReducer = (state: InitialStateType = initialState, action: PacksReducerActionTypes): InitialStateType => {
     switch (action.type) {
         case 'packs/SET-PACKS-LIST':
-            return [...state, ...action.cardPacks];
+            return {...state, packsList: [...state.packsList, ...action.cardPacks]};
+        // return [...state, ...action.cardPacks];
         case 'packs/CLEAR-PACKS-LIST':
-            return [];
+            return {...state, packsList: []};
+        // return [];
+        case 'packs/SET-REQUESTED-PACKS':
+            return {...state, requestedPacks: action.requestedPacks};
         default: {
             return state;
         }
@@ -20,77 +28,90 @@ export const packsReducer = (state: InitialStateType = initialState, action: Pac
 //actions
 export const setPacksList = (cardPacks: ResponseCardPackType[]) => ({type: 'packs/SET-PACKS-LIST', cardPacks} as const);
 export const clearPacksList = () => ({type: 'packs/CLEAR-PACKS-LIST'} as const);
+export const setRequestedPacks = (requestedPacks: RequestedPacksType) => ({
+    type: 'packs/SET-REQUESTED-PACKS',
+    requestedPacks,
+} as const);
 
 //thunks
-export const fetchPacks = (): AppThunk => (dispatch) => {
-    dispatch(setAppRequestStatusAC('loading'));
-    packsAPI.getPacks()
-        .then((res) => {
-            dispatch(setPacksList(res.data.cardPacks));
-        })
-        .catch((err: AxiosError<{ error: string }>) => {
-            const error = err.response
-                ? err.response.data.error
-                : err.message;
-            dispatch(setAppErrorAC(error));
-        })
-        .finally(() => {
-            dispatch(setAppRequestStatusAC('idle'));
-        });
+export const fetchUserPacks = (user_id: string): AppThunk => async (dispatch) => {
+    try {
+        dispatch(setAppRequestStatusAC('loading'));
+        const response = await packsAPI.getPacks({user_id});
+        const pageCount = response.data.cardPacksTotalCount;
+
+        const res = await packsAPI.getPacks({pageCount, user_id});
+        dispatch(clearPacksList());
+        dispatch(setPacksList(res.data.cardPacks));
+    } catch (e) {
+        handleServerNetworkError(e, dispatch);
+    } finally {
+        dispatch(setAppRequestStatusAC('idle'));
+    }
 };
 
-export const addPack = (): AppThunk => (dispatch) => {
-    dispatch(setAppRequestStatusAC('loading'));
-    packsAPI.createPack()
-        .then((res) => {
-            dispatch(clearPacksList());
-            dispatch(fetchPacks());
-        })
-        .catch((err: AxiosError<{ error: string }>) => {
-            const error = err.response
-                ? err.response.data.error
-                : err.message;
-            dispatch(setAppErrorAC(error));
-        })
-        .finally(() => {
-            dispatch(setAppRequestStatusAC('idle'));
-        });
+export const fetchAllPacks = (): AppThunk => async (dispatch) => {
+    try {
+        dispatch(setAppRequestStatusAC('loading'));
+        const response = await packsAPI.getPacks();
+        const pageCount = response.data.cardPacksTotalCount;
+
+        const res = await packsAPI.getPacks({pageCount});
+        dispatch(clearPacksList());
+        dispatch(setPacksList(res.data.cardPacks));
+    } catch (e) {
+        handleServerNetworkError(e, dispatch);
+    } finally {
+        dispatch(setAppRequestStatusAC('idle'));
+    }
 };
 
-export const removePack = (packID: string): AppThunk => (dispatch) => {
-    dispatch(setAppRequestStatusAC('loading'));
-    packsAPI.deletePack(packID)
-        .then((res) => {
-            dispatch(clearPacksList());
-            dispatch(fetchPacks());
-        })
-        .catch((err: AxiosError<{ error: string }>) => {
-            const error = err.response
-                ? err.response.data.error
-                : err.message;
-            dispatch(setAppErrorAC(error));
-        })
-        .finally(() => {
-            dispatch(setAppRequestStatusAC('idle'));
-        });
+export const addPack = (user_id: string, requestedPacks: RequestedPacksType): AppThunk => async (dispatch) => {
+    try {
+        dispatch(setAppRequestStatusAC('loading'));
+        await packsAPI.createPack();
+        if (requestedPacks === `User's`) {
+            await dispatch(fetchUserPacks(user_id));
+        } else {
+            await dispatch(fetchAllPacks());
+        }
+    } catch (e) {
+        handleServerNetworkError(e, dispatch);
+    } finally {
+        dispatch(setAppRequestStatusAC('idle'));
+    }
 };
 
-export const changePack = (data: UpdatePackParamsType): AppThunk => (dispatch) => {
-    dispatch(setAppRequestStatusAC('loading'));
-    packsAPI.updatePack(data)
-        .then((res) => {
-            dispatch(clearPacksList());
-            dispatch(fetchPacks());
-        })
-        .catch((err: AxiosError<{ error: string }>) => {
-            const error = err.response
-                ? err.response.data.error
-                : err.message;
-            dispatch(setAppErrorAC(error));
-        })
-        .finally(() => {
-            dispatch(setAppRequestStatusAC('idle'));
-        });
+export const removePack = (packID: string, user_id: string, requestedPacks: RequestedPacksType): AppThunk => async (dispatch) => {
+    try {
+        dispatch(setAppRequestStatusAC('loading'));
+        await packsAPI.deletePack(packID);
+        if (requestedPacks === `User's`) {
+            await dispatch(fetchUserPacks(user_id));
+        } else {
+            await dispatch(fetchAllPacks());
+        }
+    } catch (e) {
+        handleServerNetworkError(e, dispatch);
+    } finally {
+        dispatch(setAppRequestStatusAC('idle'));
+    }
+};
+
+export const changePack = (data: UpdatePackParamsType, user_id: string, requestedPacks: RequestedPacksType): AppThunk => async (dispatch) => {
+    try {
+        dispatch(setAppRequestStatusAC('loading'));
+        await packsAPI.updatePack(data);
+        if (requestedPacks === `User's`) {
+            await dispatch(fetchUserPacks(user_id));
+        } else {
+            await dispatch(fetchAllPacks());
+        }
+    } catch (e) {
+        handleServerNetworkError(e, dispatch);
+    } finally {
+        dispatch(setAppRequestStatusAC('idle'));
+    }
 };
 
 // types
@@ -98,6 +119,10 @@ type InitialStateType = typeof initialState
 
 type setPacksListType = ReturnType<typeof setPacksList>
 type clearPacksListType = ReturnType<typeof clearPacksList>
+type setRequestedPacksType = ReturnType<typeof setRequestedPacks>
 
 export type PacksReducerActionTypes = setPacksListType
     | clearPacksListType
+    | setRequestedPacksType
+
+export type RequestedPacksType = `User's` | 'All'
